d114450fe30ea7b23df831adf0cb22c08c4f8322
Joshua119 <jmsulliv@DESKTOP-I1CPABR.localdomain>
2022-03-15 17:57:23 -0400

    initial tabs

---
+++ js/tabs.js
@@ -0,0 +1,19 @@
+function tabshift(evt, tabName) {
+    var i, tabcontent, tablinks;
+  
+    // hides unused tab content
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // De-highlights unused tabs"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Displays tab, hilights active tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
