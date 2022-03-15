02751565c80098d442434822b2d7f01c36c37b93
Demetri Cassidy <demetricassidy99@gmail.com>
2022-03-15 19:11:45 -0400

    Moved "js" and "css" directories into "public" directories so server and deliver index.html properly

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var express = require("express");
 var app = express();
 
-// serves static files in ./public/ directory
-app.use(express.static('public'))
+// serves static files in ./public/ directory and its subdirectories
+app.use(express.static(__dirname + "/public"));
 
 app.get('/', (req, res) => {
     //res.send("Hello, world!");
 })
 
 app.listen(8000, () => {
-    console.log("Listening on port 8000...\n Press Ctrl-C to stop server\n");
+    console.log("Listening on port 8000...\nPress Ctrl-C to stop server\n");
 });
