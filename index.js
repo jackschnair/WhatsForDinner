252264e088dbae7e4a9699033827626a3e4bc8f5
Demetri Cassidy <demetricassidy99@gmail.com>
2022-03-15 17:50:58 -0400

    minor edits to index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require("express");
 var app = express();
 
+// serves static files in ./public/ directory
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    res.send("Hello, world!");
+    //res.send("Hello, world!");
 })
 
 app.listen(8000, () => {
-    console.log("Listening on port 8000...\n");
+    console.log("Listening on port 8000...\n Press Ctrl-C to stop server\n");
 });
