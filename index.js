5dd61058b80fe45116149fd6b0d7459544f00051
DemetriCassidy <demetricassidy99@gmail.com>
2022-03-24 20:18:55 -0400

    updated how app uses ports so heroku is happy

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var express = require("express");
 var app = express();
+var PORT = (process.env.PORT || 8000);
 
 // serves static files in ./public/ directory and its subdirectories
 app.use(express.static(__dirname + "/public"));
+
+app.set('port', PORT);
 
 app.get('/', (req, res) => {
     //res.send("Hello, world!");
 })
 
-app.listen(8000, () => {
-    console.log("Listening on port 8000...\nPress Ctrl-C to stop server\n");
+app.listen(app.get('port'), () => {
+    console.log("What's For Dinner is running and listening on port", 
+        app.get('port'));
 });
